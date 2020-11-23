38b88980993b60b957f24801b228c08676700878
laojianzi <laojianzi1994@gmail.com>
2020-11-23 22:48:39 +0800

    repo: add examples test

--- examples/test/test.go
+++ examples/test/test.go
@@ -1,1 +1,28 @@
-package test
+package main
+
+import (
+	"fmt"
+	"image/png"
+	"log"
+	"os"
+	"time"
+
+	"github.com/laojianzi/mdavatar"
+)
+
+func main() {
+	initials := "LR"
+	avatar, err := mdavatar.New(initials).Build()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filename := fmt.Sprintf("out-%d.png", time.Now().Unix())
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := png.Encode(file, avatar); err != nil {
+		panic(err)
+	}
+}
